7c7acd793452ef1f5a7710441c084d54797cedf8
naufalafles <zamrinaufalcode@gmail.com>
2023-12-27 01:30:34 +1100

    update source code
    File & Folder Structre incld index.css google url correction

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,8 +1,20 @@
 import React from 'react';
 
+import './App.scss';
+import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components';
+
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <Navbar />
+      <About />
+      <Footer />
+      <Header />
+      <Skills />
+      <Testimonial />
+      <Work />
+    </div>
   );
 }
 
